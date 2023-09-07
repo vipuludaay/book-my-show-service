226027493b5ea89509423a1fb85e2c2bbc19d745
Vipul Kumar <vipvipul1@gmail.com>
2023-09-07 09:42:04 +0530

    removed unused code

--- src/main/java/org/vip/bookmyshow/BookMyShowApplication.java
+++ src/main/java/org/vip/bookmyshow/BookMyShowApplication.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
-//@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class BookMyShowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
